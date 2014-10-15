64bda3a57709a9449702eeaa6d0bd099a1608f4e
Wesley Moore <wmoore@jlab.org>
2014-10-15 11:16:58 -0400

    Working tempSensApp for Omegabus thermocouples

---
+++ apps/iocBoot/ioctempSens/st.cmd
@@ -0,0 +1,33 @@
+#!../../bin/linux-x86/tempSens
+
+## You may have to change tempSens to something else
+## everywhere it appears in this file
+
+< envPaths
+
+cd ${TOP}
+
+epicsEnvSet("STREAM_PROTOCOL_PATH", "${TOP}/proto")
+
+## Register all support components
+dbLoadDatabase("dbd/tempSens.dbd")
+tempSens_registerRecordDeviceDriver(pdbbase)
+
+## Setup port
+drvAsynIPPortConfigure("SER1", "hallb-moxa1:4001")
+
+## Debugging
+asynSetTraceMask("SER1",-1,0x09)
+asynSetTraceIOMask("SER1",-1,0x2)
+
+## Load record instances
+#dbLoadTemplate("db/iocAdminSoft.db, "IOC=${IOC}")
+#dbLoadRecords("db/save_restoreStatus.db", "P=${IOC}:")
+dbLoadTemplate("db/tempSens.substitutions")
+
+cd ${TOP}/iocBoot/${IOC}
+
+#< save_restore.cmd
+
+dbl > tempSens_pv.list
+iocInit
