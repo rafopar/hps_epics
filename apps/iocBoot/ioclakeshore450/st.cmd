27bf1d6b00ed568a08867aaff101e74fe577228a
Wesley Moore <wmoore@jlab.org>
2014-10-29 20:19:03 -0400

    Features added to match old app

--- apps/iocBoot/ioclakeshore450/st.cmd
+++ apps/iocBoot/ioclakeshore450/st.cmd
@@ -4,25 +4,39 @@
 ## everywhere it appears in this file
 
 < envPaths
-
 cd ${TOP}
 
+epicsEnvSet("STREAM_PROTOCOL_PATH", "${TOP}/proto")
+
 ## Register all support components
-dbLoadDatabase "dbd/lakeshore450.dbd"
-lakeshore450_registerRecordDeviceDriver pdbbase
+dbLoadDatabase("dbd/lakeshore450.dbd")
+lakeshore450_registerRecordDeviceDriver(pdbbase)
+
+## Setup port
+drvAsynIPPortConfigure("SER3", "hallb-moxa1:4003")
+drvAsynIPPortConfigure("SER4", "hallb-moxa1:4004")
+
+## Set this to see debug messages from asyn
+#asynSetTraceMask("SER3",-1,0x09)
+#asynSetTraceIOMask("SER3",-1,0x2)
+#asynSetTraceMask("SER4",-1,0x09)
+#asynSetTraceIOMask("SER4",-1,0x2)
 
 ## Load record instances
-dbLoadTemplate "db/userHost.substitutions"
-dbLoadRecords "db/dbSubExample.db", "user=wmooreHost"
-
-## Set this to see messages from mySub
-#var mySubDebug 1
-
-## Run this to trace the stages of iocInit
-#traceIocInit
+dbLoadRecords("db/iocAdminSoft.db", "IOC=${IOC}")
+dbLoadRecords("db/save_restoreStatus.db", "P=${IOC}:")
+dbLoadRecords("db/lakeshore450.db", "P=HPS:, R=LS450_1:, PORT=SER3")
+dbLoadRecords("db/lakeshore450.db", "P=HPS:, R=LS450_2:, PORT=SER4")
 
 cd ${TOP}/iocBoot/${IOC}
+
+## Autosave settings/restore sets
+< save_restore.cmd
+
+dbl > lakeshore450_pv.list
 iocInit
 
-## Start any sequence programs
-#seq sncExample, "user=wmooreHost"
+## Autosave startup
+makeAutosaveFiles()
+create_monitor_set("info_positions.req", 5, "P=${IOC}:")
+create_monitor_set("info_settings.req", 30, "P=${IOC}:")
